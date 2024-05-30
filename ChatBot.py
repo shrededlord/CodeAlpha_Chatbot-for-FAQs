72fd7f237527c27cf519a54bfd98df711617fadd
shrededlord <153372144+shrededlord@users.noreply.github.com>
2024-05-30 21:15:03 +0300

    added lemmatization and greetings

--- ChatBot.py
+++ ChatBot.py
@@ -15,3 +15,21 @@ sent_tokens = nltk.sent_tokenize(raw)# converts to List of sentences
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
 
 
+lemmer = nltk.stem.WordNetLemmatizer()
+#wordNet is a semantically-oriented dictionary of English included in NLTK.
+def LemTokens (tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+
+GREETING_INPUTS=("hello", "hi", "greetings", "sup", "what's up", "hey")
+GREETING_RESPONSES=["hi there", "hey", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting (sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+        
